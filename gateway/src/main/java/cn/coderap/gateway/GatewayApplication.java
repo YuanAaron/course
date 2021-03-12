b1f375b62ff6e1a80a355515aed1c5b4677e5e4b
oshacker <1500438364@qq.com>
2021-03-12 17:20:32 +0800

    1、新增gateway模块，增加gateway依赖，并注册到注册中心

---
+++ gateway/src/main/java/cn/coderap/gateway/GatewayApplication.java
@@ -0,0 +1,25 @@
+package cn.coderap.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(GatewayApplication.class);
+
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(GatewayApplication.class);
+		ConfigurableEnvironment env = app.run(args).getEnvironment();
+		//启动日志可以按需扩展，甚至可以打印一些有趣的字符图案
+		LOGGER.info("启动成功!");
+		LOGGER.info("Gateway地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
+	}
+
+}

2f66c3f273ceebcb2085063b44f2a424519ae942
oshacker <1500438364@qq.com>
2021-03-25 15:21:01 +0800

    1、前端分页组件及使用

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <p>
-      <button v-on:click="list()" class="btn btn-white btn-default btn-round">
+      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
         <!--fa(fontawesome)：图标样式-->
         <i class="ace-icon fa fa-refresh red2"></i>
         刷新
       </button>
     </p>
+    <!--
+    在JS中需要通过document.querySelector("#demo")来获取dom节点，然后再获取这个节点的值。
+    在Vue中，我们不用获取dom节点，元素绑定ref之后(取别名)，直接通过this.$refs.refName即可调用，这样可以减少获取dom节点的消耗。
+    关于Vue中的ref和refs参考：https://blog.csdn.net/qq_38128179/article/details/88876060
+    -->
+    <pagination ref="pagination" v-bind:list="list"></pagination>
     <!--
     删除多余的row和col-xs-12:
     两者都是bootstrap栅格系统的内置样式，用于响应式页面的布局
@@ -86,7 +92,10 @@
 </template>
 
 <script>
+import Pagination from "../../components/pagination";
+
 export default {
+  components: {Pagination},
   name: 'chapter',
   data: function () {
     return {
@@ -98,19 +107,20 @@ export default {
     //子组件welcome调用父组件admin的方法，可以使用$parent
     //sidebar激活样式方法一
     // this.$parent.activeSideBar("business-chapter-sidebar");
-    _this.list();
+    _this.list(1);
 
   },
   methods: {
     //接口请求参数传递，尽量使用post：使用get请求在url里拼接参数的话，会使url变得很长，有些浏览器或服务器会对url长度做限制，导致请求失败
-    list() {
+    list(page) {
       let _this = this;
       _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list',{
-        page: 1,
-        size: 1
+        page: page,
+        size: _this.$refs.pagination.size
       }).then(res => {
         // console.log("查询大章列表结果为：",res);
         _this.chapters = res.data.list;
+        _this.$refs.pagination.render(page,res.data.total);
       })
     }
   }

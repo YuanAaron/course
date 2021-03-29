afca1a6c5abb2029bbebe165fafac6272fd50199
oshacker <1500438364@qq.com>
2021-03-29 13:28:02 +0800

    1、如果开发过程中提示”请求参数错误“，说明后端有校验，前端没有，此时应该把前端校验加上

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -193,11 +193,11 @@ export default {
       let _this = this;
 
       //校验
-      // if (!Validator.require(_this.chapter.name,"名称")
-      //   || !Validator.require(_this.chapter.courseId,"课程ID")
-      //   || !Validator.length(_this.chapter.courseId,"课程ID",1,8)) {
-      //   return;
-      // }
+      if (!Validator.require(_this.chapter.name,"名称")
+        || !Validator.require(_this.chapter.courseId,"课程ID")
+        || !Validator.length(_this.chapter.courseId,"课程ID",1,8)) {
+        return;
+      }
 
       Loading.show();
       _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save',_this.chapter).then(res => {

e9775b58320475eb58628f1e54df7c87a66f291e
oshacker <1500438364@qq.com>
2021-03-30 14:24:40 +0800

    1、代码生成器：小节的表section结构设计和生成section的持久层代码

--- doc/sql/all.sql
+++ doc/sql/all.sql
@@ -1,3 +1,24 @@
+-- 小节
+DROP TABLE IF EXISTS `section`;
+CREATE TABLE `section`
+(
+    `id`         CHAR(8)     NOT NULL DEFAULT '' COMMENT 'ID',
+    `title`      VARCHAR(50) NOT NULL COMMENT '标题',
+    `course_id`  CHAR(8)              DEFAULT NULL COMMENT '课程|course.id',
+    `chapter_id` CHAR(8)              DEFAULT NULL COMMENT '大章|chapter.id',
+    `video`      VARCHAR(200)         DEFAULT NULL COMMENT '视频',
+    `time`       INT(11)              DEFAULT NULL COMMENT '时长|单位秒',
+    `charge`     CHAR(1)              DEFAULT NULL COMMENT '收费|C 收费；F 免费',
+    `sort`       INT(11)              DEFAULT NULL COMMENT '顺序',
+    `created_at` datetime(3)          DEFAULT NULL COMMENT '创建时间',
+    `updated_at` datetime(3)          DEFAULT NULL COMMENT '修改时间',
+    PRIMARY KEY (`id`)
+) ENGINE = INNODB
+  DEFAULT CHARSET = utf8mb4 COMMENT = '小节';
+
+INSERT INTO  `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, updated_at)
+VALUES ('00000001', '测试小节01', '00000001', '00000000', '', 500, 'F', 1, now(), now());
+
 DROP TABLE IF EXISTS `chapter`;
 CREATE TABLE `chapter`
 (
